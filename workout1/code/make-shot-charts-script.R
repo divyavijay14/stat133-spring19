09c40ad373f82c19866c3a1d83f1e47bb834bc6c
Divya Vijay <divyavijay@berkeley.edu>
2019-03-13 23:56:12 -0700

    last one

--- workout1/code/make-shot-charts-script.R
+++ workout1/code/make-shot-charts-script.R
@@ -4,7 +4,6 @@
 # output(s): This file will output multiple shot charts.
 
 library(ggplot2)
-install.packages('jpeg')
 library(jpeg)
 library(grid)
 
@@ -30,58 +29,56 @@ klay_shot_chart <- ggplot(data = thompson) +
 klay_shot_chart
 
 # stephen curry shot chart
-{
 stephen_curry_shot_chart <- ggplot(data = curry) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
   ylim(-50, 420) +
   ggtitle('Shot Chart: Steph Curry (2016 season)') +
   theme_minimal()
-pdf(file = "../images/stephen-curry-shot-chart.pdf", 6.5, 5)
-}
-stephen_curry_shot_chart
-# draymond green shot chart
-{
+ggsave('../images/stephen-curry-shot-chart.pdf', plot = stephen_curry_shot_chart)
+
+  # draymond green shot chart
+
 draymond_green_shot_chart <- ggplot(data = green) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
   ylim(-50, 420) +
   ggtitle('Shot Chart: Draymond Green (2016 season)') +
   theme_minimal()
-pdf(file = "../images/draymond-green-shot-chart.pdf", 6.5, 5)
-}
+ggsave('../images/draymond-green-shot-chart.pdf', plot = draymond_green_shot_chart)
+
 # kevin durant shot chart
-{
 kevin_durant_shot_chart <- ggplot(data = durant) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
   ylim(-50, 420) +
   ggtitle('Shot Chart: Kevin Durant (2016 season)') +
   theme_minimal()
-pdf(file = "../images/kevin-durant-shot-chart.pdf", 6.5, 5)
-}
+ggsave('../images/kevin-durant-shot-chart.pdf', plot = kevin_durant_shot_chart)
+
 # klay thompson shot chart
-{
+
 klay_thompson_shot_chart <- ggplot(data = thompson) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
   ylim(-50, 420) +
   ggtitle('Shot Chart: Klay Thompson (2016 season)') +
   theme_minimal()
-pdf(file = "../images/klay-thompson-shot-chart.pdf", 6.5, 5)
-}
+ggsave('../images/klay-thompson-shot-chart.pdf', plot = klay_thompson_shot_chart)
+
+
 # andre iguodala shot chart
-{
+
 andre_iguodala_shot_chart <- ggplot(data = iguodala) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
   ylim(-50, 420) +
   ggtitle('Shot Chart: Andre Iguodala (2016 season)') +
   theme_minimal()
-pdf(file = '../images/andre-iguodala-shot-chart.pdf', 6.5, 5)
-}
+ggsave('../images/andre-iguodala-shot-chart.pdf', plot = andre_iguodala_shot_chart)
 
-{
+
+
 gsw_shot_charts <- ggplot(data = shots_data) +
   annotation_custom(court_image, -250, 250, -50, 420) +
   geom_point(aes(x = x, y = y, color = shot_made_flag)) +
@@ -91,9 +88,8 @@ gsw_shot_charts <- ggplot(data = shots_data) +
             theme_minimal())
 
 gsw_shot_charts
-pdf(file = '../images/gsw-shot-charts.pdf', 8, 7)
+ggsave('../images/gsw-shot-charts.pdf', plot = gsw_shot_charts)
+ggsave('../images/gsw-shot-charts.png', plot = gsw_shot_charts)
 
-png(file = '../images/gsw-shot-charts.png', 8, 7)
-}
 
 
